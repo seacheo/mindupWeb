4a9d0d2b6cbb26ed6e235b456a4e0bf56c9619e5
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-09-25 22:33:46 -0400

    Mission Statement Updated

--- src/pages/Landing/Mission.jsx
+++ src/pages/Landing/Mission.jsx
@@ -21,11 +21,11 @@ const Mission = () => {
             <img src={squiggle} style={{transform: "scaleY(-1.1) scaleX(1.2)", width: "100%", position: 'absolute', top: "-1px", fill: "red"}}></img>
 
             <div style={{display: "flex", justifyContent:  mobile ? "center" : "flex-end", alignItems: 'center', height: "100%", padding: mobile ? "20px 0px": "0px 100px 0px 0px" }}>
-                <Card style={{width: mobile ? "350px" : "550px", padding: mobile ? "12px 20px": "24px 50px",boxShadow: "2px 4px 10px rgba(0,0,0,0.1)", marginTop: "20px"}}>
+                <Card style={{width: mobile ? "350px" : "550px", padding: mobile ? "20px 20px": "36px 50px",boxShadow: "2px 4px 10px rgba(0,0,0,0.1)", marginTop: "20px"}}>
                     <Title level={2} style={{color: "#13384e"}}> Our Mission. </Title>
 
-                    <Paragraph style={{lineHeight: "1.9", textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit rem qui in velit architecto, rerum saepe reprehenderit delectus aliquid quo facere praesentium culpa cupiditate. Sequi id consectetur nostrum quas similique expedita perferendis magnam, voluptate labore earum rerum quis! Ipsam qui quia temporibus excepturi ad nobis accusantium eligendi sapiente nulla. Non?</Paragraph>
-                    <Paragraph style={{lineHeight: "1.9", textAlign: 'justify'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit rem qui in velit architecto, rerum saepe reprehenderit delectus aliquid quo facere praesentium culpa cupiditate. Sequi id consectetur nostrum quas similique expedita perferendis magnam, voluptate labore earum rerum quis! Ipsam qui quia temporibus excepturi ad nobis accusantium eligendi sapiente nulla. Non?</Paragraph>
+                    <Paragraph style={{lineHeight: "1.9", textAlign: 'justify'}}>Our neuroadaptive breathing entrainment system serves to identify an ideal breathing rhythm for a user to effectively address anxiety in real-time. Our system is adaptive, taking into account the output of the deep learning algorithm to determine whether a particular breathing rate has an alleviating effect on the user’s mental state, and adapting accordingly until ultimately finding the user’s ideal breathing rate. 
+</Paragraph>
 
                 </Card>
             </div>
