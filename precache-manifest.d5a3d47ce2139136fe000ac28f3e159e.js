14bf488f51bf11275dda007f83e620d553f5212c
Sean Cheong <seancheong@seanc.local>
2020-09-25 21:42:49 -0700

    Updates

---
+++ precache-manifest.d5a3d47ce2139136fe000ac28f3e159e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3752b5f54c4e1b1622e2c67dca304f",
+    "url": "//sean8322.github.io/mindupWeb/index.html"
+  },
+  {
+    "revision": "18965219e80dc1b95610",
+    "url": "//sean8322.github.io/mindupWeb/static/css/main.5134a8a5.chunk.css"
+  },
+  {
+    "revision": "36f45d295ea6c9dba0b3",
+    "url": "//sean8322.github.io/mindupWeb/static/js/2.c9dd42ca.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "//sean8322.github.io/mindupWeb/static/js/2.c9dd42ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18965219e80dc1b95610",
+    "url": "//sean8322.github.io/mindupWeb/static/js/main.d22c5bd8.chunk.js"
+  },
+  {
+    "revision": "67c146d6a087b0c5451e",
+    "url": "//sean8322.github.io/mindupWeb/static/js/runtime-main.df7f5d4a.js"
+  },
+  {
+    "revision": "c7b74611baad3b5347fd6c785c012b20",
+    "url": "//sean8322.github.io/mindupWeb/static/media/avi.c7b74611.png"
+  },
+  {
+    "revision": "2bffaf55fe4813c0d6e05f24a6ce66b1",
+    "url": "//sean8322.github.io/mindupWeb/static/media/bg-image.2bffaf55.png"
+  },
+  {
+    "revision": "059272458b26b797de5d782e4460933d",
+    "url": "//sean8322.github.io/mindupWeb/static/media/brian.05927245.png"
+  },
+  {
+    "revision": "039ae07bd8e59a165bce6e643ac7bd66",
+    "url": "//sean8322.github.io/mindupWeb/static/media/fancy-squiggle.039ae07b.svg"
+  },
+  {
+    "revision": "e8126318430bdbb0ddc7dac13d06b59f",
+    "url": "//sean8322.github.io/mindupWeb/static/media/fastily-logo.e8126318.png"
+  },
+  {
+    "revision": "c51db19098cc42d8c0ebbe492764b0e4",
+    "url": "//sean8322.github.io/mindupWeb/static/media/features.c51db190.png"
+  },
+  {
+    "revision": "49dbb9f53b81a9c2219fbf49cc1ae0df",
+    "url": "//sean8322.github.io/mindupWeb/static/media/mindup-logo.49dbb9f5.png"
+  },
+  {
+    "revision": "1dd3e63c5e4867fd47c6df94d07184a5",
+    "url": "//sean8322.github.io/mindupWeb/static/media/mockup.1dd3e63c.gif"
+  },
+  {
+    "revision": "576de3d7af470272efddb9b28ca8148f",
+    "url": "//sean8322.github.io/mindupWeb/static/media/openbci-logo.576de3d7.png"
+  },
+  {
+    "revision": "483c96ff2ba42610eb29339496b8213b",
+    "url": "//sean8322.github.io/mindupWeb/static/media/pattern.483c96ff.svg"
+  },
+  {
+    "revision": "16dd7cb3809c6751631fb89b14068e3c",
+    "url": "//sean8322.github.io/mindupWeb/static/media/sean.16dd7cb3.png"
+  }
+]);
