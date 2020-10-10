b8018f3f652b99c611f8701c11bc68b8f84d574e
Sean Cheong <seancheong@seanc.local>
2020-10-10 11:47:40 -0700

    added pictures

--- src/pages/Landing/Photos.jsx
+++ src/pages/Landing/Photos.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import {Typography, Image} from 'antd'
-
+import pic1 from '../../assets/product/BW1.JPG'
+import pic2 from '../../assets/product/BW2.png'
 const {Title, Text} = Typography
 
 
@@ -10,8 +11,8 @@ const Photos = () => {
     return (
         <div >
         <div style={{ display: "flex", justifyContent: 'center', flexWrap: "wrap", width: '100%', marginTop: "30px"}}>
-            <img src="https://via.placeholder.com/350" style={{borderRadius: "10px", margin: "20px 50px"}}></img>
-            <img src="https://via.placeholder.com/350" style={{borderRadius: "10px", margin: "20px 50px"}}></img>
+            <img src={pic1} style={{width: "50%", maxWidth: "800px", borderRadius: "10px", margin: "20px 50px"}}></img>
+            <img src={pic2} style={{width: "50%", maxWidth: "800px", borderRadius: "10px", margin: "20px 50px"}}></img>
         </div>
     </div>
     )
--- src/pages/Landing/Team.jsx
+++ src/pages/Landing/Team.jsx
@@ -18,8 +18,8 @@ const Team =() => {
             <Title level={2} style={{color: "#13384e", marginTop: "30px", textAlign: "center"}}> Team </Title>
             <div style={{ display: "flex", justifyContent: 'center', flexWrap: "wrap"}}>
                 <TeamMember info={{name: "Brian Penchina", role: "Backend | ML", linkedIn: "brian-penchina", img: brian}}/>
-                <TeamMember info={{name: "Sean Cheong", role: "Backend | ML", linkedIn: "brian-penchina", img: sean}}/>
-                <TeamMember info={{name: "Avi Curryasan", role: "Backend | ML", linkedIn: "brian-penchina", img: avi}}/>
+                <TeamMember info={{name: "Sean Cheong", role: "Backend | ML", linkedIn: "sean-cheong", img: sean}}/>
+                <TeamMember info={{name: "Avi Sundaresan", role: "Backend | ML", linkedIn: "avirathsundaresan", img: avi}}/>
             </div>
         </div>
     )
